9cfeb5476ee23ce6819f5cd120ed691349e30610
Ubaydul Haque Sami <64384070+samihaque@users.noreply.github.com>
2023-02-24 15:43:59 -0500

    added winning-tie logic and change header to show message accordingly

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X'
+let win;
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
+// variable to change top message which indicates correct turn
+const messages = document.querySelector('h2');
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
@@ -23,7 +36,7 @@ function rander(){
         //this sets the text content of the square of the same index to the mark on the board.
         squares[index].textContent = mark;
     });
-
+    messages.textContent = win ==='T'? `That's a Tie!!`: win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 }
 
 // declaring a function to handle turn
@@ -33,8 +46,19 @@ function handleTurn(event){
     })
     board[idx] = turn;
     turn = turn === 'X'? 'O': 'X';
+    win = getWinner();
     rander();
     console.log(board)
+}
+
+// finding out the winner
+function getWinner(){
+    let winner = null;
+    winningCombos.forEach(function(combo, index){
+        if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]])
+            winner= board[combo[0]];
+    })
+    return winner ? winner : board.includes('') ? null : 'T';
 }
 
 //calling the init function!
